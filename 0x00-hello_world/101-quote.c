e3461671767228635779b3d902571525dfbc8560
Kyrillos77 <kironabil86@gmail.com>
2023-02-16 08:56:04 -0800

    finished

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
-int main() {
-    
-   printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19 \n");
-   
-   
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
 
-    return 1;
+	return (1);
 }
